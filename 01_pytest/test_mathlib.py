19519db2c02bc0a66cbbd7ed2f9e2b49a111ae31
peterhchen <peter.hp.chen@gmail.com>
2019-05-29 13:33:01 -0700

    introduction to pytest

---
+++ 01_pytest/test_mathlib.py
@@ -0,0 +1,11 @@
+import mathlib
+
+# pytest uses some dicovery rule for the test
+# "test_" is one of them.
+def test_calc_total():
+    total = mathlib.calc_total (4, 5)
+    assert total == 9
+
+def test_calc_multiply ():
+    result = mathlib.calc_multiply (10, 3)
+    assert result == 30
